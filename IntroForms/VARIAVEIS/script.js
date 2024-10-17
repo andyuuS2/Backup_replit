c82f24fd720b227ee25b9f92c88ee318e7d14af3
andyuuS2 <maria.e.souza1509@gmail.com>
2024-10-17 11:20:54 -0300

    introforms/form_color/variaveis

---
+++ IntroForms/VARIAVEIS/script.js
@@ -0,0 +1,11 @@
+//# id ou identificador
+//. class ou classe css
+const comidas = document.querySelector('.comidas')
+
+//prompt pergunta ao usuario (interage)
+const comida1 = prompt('Digite sua comida nº1')
+
+//insere o conteudo do usurio dentro do html
+comidas.innerText = comida1;
+
+//alert("Você gosta de:" + comida1)
