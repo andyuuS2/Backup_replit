6e5440c65ec669cac1eb71137d1dcbb462bba3ae
andyuuS2 <maria.e.souza1509@gmail.com>
2024-10-17 11:18:08 -0300

    JMOBILE

---
+++ JSMOBILE/IF_ELSE/ifElse.js
@@ -0,0 +1,16 @@
+//CASE SENSITIVE - Maiusculas diferem de minusculas
+const cor = prompt("Qual sua cor preferida")
+
+if (cor == "vermelho") {
+  console.log('Vermelho')
+}
+
+else if (cor == "azul") {
+  console.log('Azul')
+}
+
+else {
+  console.log("Não conheço essa cor");
+}
+
+console.log(cor.length)
